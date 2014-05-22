bd1c013294ee7e0d143b00c05f9c5212016b2e8b
F.J. Valverde <fva@tsc.uc3m.es>
2014-05-23 00:02:00 +0200

    Fixed some problem with diff

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-<<<<<<< HEAD
 ## This function resembles the makeVector, function:
 ## It is to be used in two steps: first store the data with set(data) then
 ## obtain the inverse for the first time with solve or else get it from 
@@ -22,18 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
-=======
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
 }
 
 
-## Write a short comment describing this function
-<<<<<<< HEAD
-## Follows the pattern of cachemean:
+## CacheSolve follows the pattern of cachemean:
 ## - takes an extended matrix object from the previous function and tries to 
 ## lookup the cached inverse.
 ## - if it has not been previously calculated, then do and return the inverse.
@@ -54,10 +42,4 @@ cacheSolve <- function(x, ...) {
 ## Some test code
 eye <- 2 * diag(3)
 ceye <- makeCacheMatrix(eye)
-iceye <- cacheSolve(ceye)
-=======
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
+cacheSolve(ceye)
